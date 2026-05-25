6b5a8acfefcbf332359da272e7d0f25fc98bc4b9
dattuwiss <kovalchuk.darial@example.com>
2026-05-25 21:49:24 +0300

    feat(practical-5): solve kangaroo challenge with unit tests

---
+++ src/hackerrank/task5.rs
@@ -0,0 +1,23 @@
+pub fn kangaroo(x1: i32, v1: i32, x2: i32, v2: i32) -> String {
+    if v1 <= v2 {
+        return String::from("NO");
+    }
+    if (x2 - x1) % (v1 - v2) == 0 {
+        String::from("YES")
+    } else {
+        String::from("NO")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_kangaroo_meet() {
+        assert_eq(kangaroo(0, 3, 4, 2), "YES");
+    }
+    #[test]
+    fn test_kangaroo_never_meet() {
+        assert_eq(kangaroo(0, 2, 5, 3), "NO");
+    }
+}
